b827b776cb9b121ed5fbeb27eb65b878f787501c
jitendra_yadav <jitendrayadav200317@gmaol.com>
2026-06-08 20:32:59 +0530

    add

--- server/app.js
+++ server/app.js
@@ -7,21 +7,18 @@ import admin from "firebase-admin";
 import dbConnect from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
-/* ✅ dotenv SABSE UPAR */
 dotenv.config();
-
+dbConnect();
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true,
-  })
+  }),
 );
 
-/* ✅ Firebase service account AFTER dotenv */
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
@@ -31,8 +28,7 @@ const serviceAccount = {
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
   token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 };
 
@@ -40,10 +36,8 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-dbConnect();
-
 app.use("/auth", userRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+  console.log(` Server running on port ${process.env.PORT}`);
 });
